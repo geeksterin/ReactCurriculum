1e023427f88e93b3f700ce408c64e6ff0d10acf7
ritikmasand <ritikmasand10@gmail.com>
2022-10-12 17:11:56 +0530

    resume updated

--- Day_03_Resume_builder/react_resume/src/Components/Header.jsx
+++ Day_03_Resume_builder/react_resume/src/Components/Header.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <div>
-         <h2>Bittoo Aggarwal</h2>
-        <p>Software Developer,Educator at geekster</p>
-  <a href="https://www.linkedin.com/feed/">
-    <li>Linkedin</li>
-    </a>
-    <a href="https://github.com/">
+      <h2>Ritik Masand</h2>
+      <p>Software Developer,Educator at geekster</p>
+      <a href="https://www.linkedin.com/feed/">
+        <li>Linkedin</li>
+      </a>
+      <a href="https://github.com/">
         <li>Github</li>
-        </a>
-        
+      </a>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
